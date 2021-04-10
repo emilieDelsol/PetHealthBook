572b6fb7c8ebc0c2625cb00048e970124147190c
emilieDelsol <70573399+emilieDelsol@users.noreply.github.com>
2021-04-10 22:47:56 +0200

    🚀 (deploy): Deploy app first step (#12)
    build app

--- front-pet-health-book/src/components/Dashboard.jsx
+++ front-pet-health-book/src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const Dashboard = () =>{
     // eslint-disable-next-line react-hooks/rules-of-hooks
 const [reports, setReports] = useState([]); 
-const url = 'http://localhost:5000/dashboard/reports';
+const url = 'http://lvh.me/dashboard/reports';
 const getReports = () => {
     axios
     .get(url)
